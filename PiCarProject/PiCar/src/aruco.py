20ac6e2163c62f087a0a4fe2f747a5b07701050f
AnupamKshetri <97123617+AnupamKshetri@users.noreply.github.com>
2024-06-11 20:26:33 +0200

    fix: Add aruco marker inner corner function

--- PiCarProject/PiCar/src/aruco.py
+++ PiCarProject/PiCar/src/aruco.py
@@ -24,6 +24,27 @@ def get_aruco_marker_corners(image):
     return []
 
 
+def get_aruco_marker_inner_corners(image):
+    marker_id = 0
+    margin_mm = 15
+    margin_rect = margin_mm * np.array([
+        [-1, -1],
+        [1, -1],
+        [1, 1],
+        [-1, 1],
+    ])
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    dictionary = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+    parameters = cv2.aruco.DetectorParameters_create()
+    corners, ids, _ = cv2.aruco.detectMarkers(gray, dictionary, parameters=parameters)
+    if ids is not None and marker_id in ids:
+        marker_index = np.where(ids == marker_id)[0][0]
+        marker_corners = corners[marker_index][0].astype(int)
+        return marker_corners
+    return []
+
+
 def draw_aruco_markers(image, outer_corners):
     for i in range(4):
         point_a = tuple(outer_corners[i])
